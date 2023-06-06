5149bc0b191ee33db11765021248e69fa3e6b0fa
taiyesi <116558144+taiyesi@users.noreply.github.com>
2023-06-06 14:20:01 +0200

    Add files via upload

---
+++ pizzaorders.py
@@ -0,0 +1,48 @@
+print("Welcome to Rainbowpig Pizza!")
+size = input("What size pizza do you want? S, M  or L: ")
+add_pepperoni = input("Do you want pepperoni? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+bill = 0
+if size == "S":
+    bill += 15
+    if add_pepperoni == "Y":
+        bill += 2
+        if extra_cheese == "Y":
+            bill += 1
+            print(f"Your totall bill is ${bill}")
+        else:
+            print(f"Your total bill is ${bill}")
+    else:
+        print(f"Your total bill is ${bill}")
+else:
+    pass
+
+if size == "M":
+    bill += 20
+    if add_pepperoni == "Y":
+        bill += 3
+        if extra_cheese == "Y":
+            bill += 1
+            print(f"Your totall bill is ${bill}")
+        else:
+            print(f"Your total bill is ${bill}")
+    else:
+        print(f"Your total bill is ${bill}")
+else:
+    pass
+
+if size == "L":
+    bill += 25
+    if add_pepperoni == "Y":
+        bill += 3
+        if extra_cheese == "Y":
+            bill += 1
+            print(f"Your totall bill is ${bill}")
+        else:
+            print(f"Your total bill is ${bill}")
+    else:
+        print(f"Your total bill is ${bill}")
+else:
+    pass
+
+
